1671dca709596e8c2d4ee3c549bfe7709f74fe2d
Jesica Ababon <jesababon@gmail.com>
2019-04-01 14:41:43 -0400

    added link and drop shadow to social buttons. centered experience in media view

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,18 @@ class Footer extends Component {
 	render() {
 		return (
 			<footer className="App-footer">
-				<img src={linkedin} className="socialBtns" alt="linkedIn" />
+
+				<a href={`https://www.linkedin.com/in/jesababon/`} target="_blank" rel="noopener noreferrer">
+				<img src={linkedin} className="socialBtns" alt="linkedin" />
+				</a>
+
+				<a href={`https://twitter.com/jesfeelit`} target="_blank" rel="noopener noreferrer">
 				<img src={twitter} className="socialBtns" alt="twitter" />
+				</a>
+
+				<a href={`https://github.com/jesababon`} target="_blank" rel="noopener noreferrer">
 				<img src={github} className="socialBtns" alt="github" />
+				</a>
 			</footer>
 		);
 	}
