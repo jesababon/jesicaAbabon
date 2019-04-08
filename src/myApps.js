a4f4b071e17db6d1837b22d8eab25ff9979e7604
Jesica Ababon <jesababon@gmail.com>
2019-04-08 16:26:26 -0400

    minor css edits

--- src/myApps.js
+++ src/myApps.js
@@ -10,24 +10,19 @@ class MyApps extends Component {
             <div><h2>Check Out My Apps</h2></div>
             <br />
             <div className="appImages">
-            <div>
+
             <a href={`https://github.com/jesababon/savvysapian`} target="_blank" rel="noopener noreferrer">
                 <img src={savvysapian} className="appImg" alt="savvysapian" />
             </a>
-            </div>
 
-            <div>
             <a href={`https://github.com/jesababon/fea`} target="_blank" rel="noopener noreferrer">
                 <img src={fatalencounters} className="appImg" alt="fatalencounters" />
             </a>
                 
-            </div>
             
-            <div>
             <a href={`https://github.com/jesababon/maddash`} target="_blank" rel="noopener noreferrer">
                 <img src={maddash} className="appImg" alt="madDash" />
             </a>    
-            </div>
             </div>
         
         </div>
