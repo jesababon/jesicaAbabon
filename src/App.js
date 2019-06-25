1bab49bab7322a7f55dab4452b4ba1d1fda9372b
Jesica Ababon <jesababon@gmail.com>
2019-06-25 15:00:24 -0400

    centered header. moved social media buttons to new footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 
 // import logo from './logo.svg';
 import Header from "./Header";
-import Social from "./Social";
 import MyApps from "./myApps";
 import Bio from "./Bio";
+import Footer from "./Footer";
 // import Contact from "./Contact";
 import "./App.css";
 import "../node_modules/modern-normalize/modern-normalize.css";
@@ -14,9 +14,9 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Header />
-				<Social />
 				<Bio />
 				<MyApps />
+				<Footer />
 				{/* <Contact /> */}
 			</div>
 		);
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Social from "./Social";
+
+
+
+class Footer extends Component {
+	render() {
+		return (
+			<footer className="App-footer">
+				<Social />
+			</footer>
+		);
+	}
+}
+
+export default Footer;
